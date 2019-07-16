4d93650c47488fb06b6ad655f58174ab366e6aa8
Riley Bridges <rlybrdgs@gmail.com>
2019-07-16 10:05:53 -0500

    Displayed ListView containing an empty event

--- app/src/main/java/com/example/medtrack/MainActivity.java
+++ app/src/main/java/com/example/medtrack/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.medtrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private List<Event> events;
+    private ArrayList<Event> events;
+    private ListView eventList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        events = new ArrayList<>();
+        events.add(new Event("empty event", "No medical events created yet", new Date()));
         EventAdapter eventListAdapter = new EventAdapter(this, R.layout.event_list_item, events);
+
+        eventList = (ListView)findViewById(R.id.event_list);
+        eventList.setAdapter(eventListAdapter);
     }
 }
